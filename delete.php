f3fc4c5e1e76e5e1ca18b0a6c464581ff9b58cd7
arunsai2002 <92771726+arunsai2002@users.noreply.github.com>
2023-03-20 11:50:41 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -11,15 +11,10 @@ header("location:login.php");
 ?>
 <?php
 //delete the posts
-if($_SESSION['message'] == "arunsainihith03@gmail.com"){
+if(isset($_SESSION['message'])){
     $conn = mysqli_connect("localhost","root","","users");
     $id = $_GET['id'];
 $result = mysqli_query($conn, "DELETE FROM `posts` WHERE id=$id");
-}
-if($_SESSION['message'] == "arunsainihith47@gmail.com"){
-    $conn = mysqli_connect("localhost","root","","users");
-    $id = $_GET['id'];
-$result = mysqli_query($conn, "DELETE FROM `posts1` WHERE id=$id");
 }
 
 ?>
