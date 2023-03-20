25bf8b2592c2d2720b50806b22d8a9cb62b0eb41
arunsai2002 <92771726+arunsai2002@users.noreply.github.com>
2023-03-20 11:50:08 +0530

    Update dislike.php

--- dislike.php
+++ dislike.php
@@ -11,7 +11,7 @@ header("location:login.php");
 ?>
 
 <?php
-if($_SESSION['message'] == "arunsainihith03@gmail.com"){
+if(isset($_SESSION['message'])){
     $conn = mysqli_connect("localhost","root","","users");
 
     $id = $_GET['id'];
@@ -29,26 +29,4 @@ if($_SESSION['message'] == "arunsainihith03@gmail.com"){
     echo "return back to home page and refresh";
 }
 
-if($_SESSION['message'] == "arunsainihith47@gmail.com"){
-    $conn = mysqli_connect("localhost","root","","users");
-
-    $id = $_GET['id'];
-    
-
-    $sql = "SELECT `dislikes` FROM `posts1` WHERE `id`=$id";
-    $result = mysqli_query($conn,$sql);
-    $row = mysqli_fetch_assoc($result);
-    $dislikes = $row['dislikes'];
-    
-    
-    $sql1 = "UPDATE `posts1` SET `dislikes` = $dislikes+1 WHERE `id`=$id";
-    $result1 = mysqli_query($conn,$sql1);
-
-    echo "return back to home page and refresh";
-}
-
-    
-
 ?>
-
-
