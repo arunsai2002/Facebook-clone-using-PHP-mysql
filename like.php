e63dbbc3e9b3e1e65197c8b8c151fe0c48ae021c
arunsai2002 <92771726+arunsai2002@users.noreply.github.com>
2023-03-20 11:49:36 +0530

    Update like.php

--- like.php
+++ like.php
@@ -10,7 +10,7 @@ header("location:login.php");
    
 ?>
 <?php
-if($_SESSION['message'] == "arunsainihith03@gmail.com"){
+if(isset($_SESSION['message'])){
 
 
 $conn = mysqli_connect("localhost","root","","users");
@@ -24,18 +24,4 @@ $conn = mysqli_connect("localhost","root","","users");
     echo "return back to home page and refresh";
 }
 
-if($_SESSION['message'] == "arunsainihith47@gmail.com"){
-    $conn = mysqli_connect("localhost","root","","users");
-
-    $id = $_GET['id'];
-    $result = mysqli_query($conn,"SELECT `likes` FROM `posts1` WHERE `id`=$id");
-    $row = mysqli_fetch_assoc($result);
-    $likes = $row['likes'];
-    $result1 = mysqli_query($conn,"UPDATE `posts1` SET `likes` = $likes+1 WHERE `id`=$id");
-    
-    echo "return back to home page and refresh";
-}
-
 ?>
-
-
